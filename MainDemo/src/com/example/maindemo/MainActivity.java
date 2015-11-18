44923305d4a4bee6015b723a760a2cb5096dc3b7
DIM <phlin163@163.com>
2015-11-18 12:01:35 +0800

    add a EditText to the view ,add the regEX

--- MainDemo/src/com/example/maindemo/MainActivity.java
+++ MainDemo/src/com/example/maindemo/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.maindemo;
 
+import java.util.regex.PatternSyntaxException;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private String TAG="Counter demo--->>";
 	private Button btnCounter=null;
+	private Button btnStop=null;
+	private TextView textVShow=null;
+	private EditText editGetNum=null;
 	private static final int COUNTER_START=1;
+	private static final int COUNTER_STOP=0;
+	private static final int NUM_ERROR=3;
 	private boolean counterFlag=false;
-	private int counter=0;
+	private int counter=100;
+	
 	Handler mHandler=new Handler()
 			{
 		
@@ -27,11 +39,18 @@ public class MainActivity extends Activity {
 				case COUNTER_START:
 					if(counterFlag)
 					{
-					counter++;
-					btnCounter.setText("count="+counter);
+					counter--;
+					textVShow.setText("count="+counter);
 					}
 					break;
-				 
+				case COUNTER_STOP:
+					counterFlag=false;
+					break;
+					
+				case NUM_ERROR:
+					textVShow.setText("num error");
+					break;
+					
 			}
 			
 		};
@@ -43,10 +62,30 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		btnCounter=(Button)findViewById(R.id.btnCounter);
+		initView();
+		 
 		
 		btnCounter.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(match(editGetNum.getText().toString()))
+				{
+				counter=Integer.parseInt(editGetNum.getText().toString());
+				Log.i(TAG, "counter="+counter);
+				counterFlag=true;
+				
+				} else
+				{
+					mHandler.sendEmptyMessage(NUM_ERROR);
+				}
+				
+				
+			}
+		});
+		
+		btnStop.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
@@ -54,9 +93,6 @@ public class MainActivity extends Activity {
 				if(counterFlag)
 				{
 					counterFlag=false;
-				}else
-				{
-					counterFlag=true;
 				}
 			}
 		});
@@ -67,7 +103,7 @@ public class MainActivity extends Activity {
 			public void run() {
 				// TODO Auto-generated method stub
 				try {
-					counterFlag=true;
+					
 					 while(true) //这个可以设置个标志位判断 退出循环。
 					 {
 						 Thread.sleep(1000);
@@ -85,6 +121,27 @@ public class MainActivity extends Activity {
 		myThread.start();
 	}
 
+	public void initView()
+	{
+		btnCounter=(Button)findViewById(R.id.btnCounter);
+		editGetNum=(EditText)findViewById(R.id.editGetNum);
+		btnStop=(Button)findViewById(R.id.btnStop);
+		textVShow=(TextView)findViewById(R.id.textVShow);
+	}
+	
+	//正则表达式 判断一下是否为整数，可以为负数。
+	public boolean match(String strSource)
+	{
+		boolean foundMatch =false;
+		try {
+			  foundMatch = strSource.matches("^-?[1-9]\\d*$");
+		} catch (PatternSyntaxException ex) {
+			// Syntax error in the regular expression
+		}
+		
+		return foundMatch;
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
